bb0fceba56cebd3ed6157a47452c42a4b8bbc48e
mokoltron <mokolotron2000@gmail.com>
2019-11-26 23:35:06 +0200

    finish

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,37 @@
 #include "Time.h"
 
 int main() {
-	Time t(555442);
-	cout << t.toSeconds();
+	cout << "t2 init as seconds \'1574791000\' " << endl;
+	Time t(1574791000);
+	cout << "t1 Seconds: " << t.toSeconds()<<endl;
+	cout << "t1 Minutes: " << t.toMinutes()<<endl;
+	
+	cout << "t2 init as string \"12 24 11\" " <<endl;
+	Time t2("12 24 11");
+
+	cout << "t3 init by constructor copy with agr t2 " << endl;
+	Time t3(t2);
+
+	cout << "t4 init by 3 number - 11, 22, 33 " << endl;
+	Time t4(11, 22, 33);
+
+	cout << endl << "t1 = " << t.toString() << " t2 = " << t2.toString()
+		<< " t3 = " << t3.toString() << " t4 = " << t4.toString() << endl;
+	
+	
+
+	bool more = t > t2;
+	cout<<endl << "t > t2 = " << boolalpha << more <<endl;
+	
+	cout << "t - t2 = " << t-t2  <<endl;
+
+	t.add(t2.toSeconds());
+	cout << "t.add(t2) = " << t.toString() <<endl;
+
+	
+
+
+
+
 	return 0;
 }
--- Time.h
+++ Time.h
@@ -1,41 +1,116 @@
 #pragma once
 #include <string>
 #include <map>
+#include <time.h>
+#include <sstream>
 using namespace std;
-//use time class
+
 class Time
 {
 private:
 	map<string, int> time;
+
+	void init(int hour, int minute, int second) {
+		this->time = {
+			{ "hour", hour},
+			{ "minute", minute },
+			{ "second", second }
+		};
+	}
+
+	tm toTm(int seconds) {
+		time_t ltime(seconds);
+		struct tm  newtime;
+		localtime_s(&newtime, &ltime);
+		return newtime;
+	}
 public:
 	const int toSeconds() {
 		 int s = (time["hour"] * 60 * 60) + time["minute"] * 60 + time["second"];
 		 return s;
 	}
 
+	int toMinutes() {
+		int s = (time["hour"] * 60 ) + time["minute"] ;
+		
+		//rounding
+		if (time["second"] >= 30)
+			s += 1;
+
+		return s;
+	}
+
 	Time(int hour, int minute, int second){
-		time = {
-			{"hour", hour},
-			{ "minute", minute },
-			{ "second", second }
-		};
+		this->init ( hour,  minute,  second);
 	}
 
 	Time(string str) {
+		istringstream stm(str);
+		string word;
+		string arr[3];
+		for (int i = 0; i < 3; i++) {
+			stm >> word;
+			arr[i] = word;
+		}
+
+		int hour = stoi(arr[0]);
+		int minute = stoi(arr[1]);
+		int second = stoi(arr[2]);
+
+		init( hour, minute, second );
 		
 	}
 
-	Time(int seconds) {
-		int hours = seconds % 60 % 60;
-		int minutes = seconds % 60;
-		int second = hours * 60 * 60 - minutes * 60;
-
-		Time(hours, minutes, second);
+	Time(int seconds){
+	    struct tm  newtime = toTm(seconds);
+		init(newtime.tm_hour, newtime.tm_min, newtime.tm_sec);
 	}
 
-	Time(  Time& obj)  {
+	Time( Time& obj)  {
 		time = obj.time;
 	}
+
+	int operator - (Time obj){
+		return this->toSeconds() - obj.toSeconds();
+	}
+
+	void add(int seconds) {
+		tm oldtime = toTm( toSeconds() );
+		tm addtime = toTm(seconds);
+
+		tm newtime = toTm((mktime(&oldtime) - mktime(&addtime)));
+		init(newtime.tm_hour, newtime.tm_min, newtime.tm_sec);
+	}
+
+
+
+	void decrease(int seconds) {
+		tm oldtime = toTm(toSeconds());
+		tm addtime = toTm(seconds);
+		tm newtime = toTm(abs(mktime(&oldtime) - mktime(&addtime)));;
+	
+		init(newtime.tm_hour, newtime.tm_min, newtime.tm_sec);
+	}
+
+	bool operator > (Time obj) {
+		if (toSeconds() > obj.toSeconds())
+			return true;
+		else 
+			return false;
+	}
+
+	string toString() {
+		
+		string s = to_string(this->time["hour"]) + ':';
+
+		s += to_string(this->time["minute"]) + ':' ;
+		s += to_string(this->time["second"]) ;
+		return s;
+	}
+
+
+
+	
 
 
 };
